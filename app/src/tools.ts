b28d1842f1cc137bea601e2507bde808d5b6af51
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-02-02 06:58:17 +0900

    Add tools

---
+++ app/src/tools.ts
@@ -0,0 +1,12 @@
+export async function fetchHtml(url: string, init?: RequestInit): Promise<Document> {
+    Object.assign(init ?? {}, { credentials: 'include', mode: 'cors' });
+    return new DOMParser().parseFromString(await (await fetch(url, init)).text(), 'text/html');
+}
+export async function fetchJson(url: string, init?: RequestInit): Promise<any> {
+    Object.assign(init ?? {}, { credentials: 'include', mode: 'cors' });
+    return JSON.parse(await (await fetch(url, init)).text());
+}
+
+export function sleep(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
