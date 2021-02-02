a26ba5a63d55f238983b9b9afaf2f8e75c0f4678
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-02-02 09:11:31 +0900

    Change type of getConfig

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,11 +1,10 @@
 const storage = browser.storage.local;
 
-export async function getConfig<T>(key: string): Promise<T> {
-    return await storage.get(key) as T;
+export async function getConfig<T>(key: string): Promise<T | undefined> {
+    return (await storage.get(key))[key] as T | undefined;
 }
 export async function setConfig<T>(key: string, value: T): Promise<void> {
     await storage.set({ [key]: value });
 }
 export async function removeConfig(key: string): Promise<void> {
     await storage.remove(key);
-}
