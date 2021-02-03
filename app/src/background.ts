f974d5c8d98cee46efa8e6cbf89c88ed4e118b50
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-02-04 03:17:33 +0900

    Add removeLoadingVideo

--- app/src/background.ts
+++ app/src/background.ts
@@ -1,10 +1,12 @@
 import { initAutoLogin } from './autoLogin/autoLogin';
 import { initConfig } from './config';
 import { MessengerServer } from './messenger';
+import { initRemoveLoadingVideo } from './removeLoadingVideo/removeLoadingVideo';
 
 export const messengerServer = new MessengerServer();
 
 (async () => {
     await initConfig();
     initAutoLogin();
+    initRemoveLoadingVideo();
 })();
---
+++ app/src/removeLoadingVideo/removeLoadingVideo.ts
@@ -0,0 +1,17 @@
+import { onConfigChange, REMOVE_LOADING_VIDEO_ENABLED } from '../config';
+
+export function initRemoveLoadingVideo(): void {
+    onConfigChange(REMOVE_LOADING_VIDEO_ENABLED, (_, newValue) => {
+        if (newValue) {
+            browser.webRequest.onBeforeRequest.addListener(redirectToEmptyVideo, { urls: ['*://*.waseda.jp/settings/viewer/uniplayer/intro.mp4'] }, ['blocking']);
+        } else {
+            browser.webRequest.onBeforeRequest.removeListener(redirectToEmptyVideo);
+        }
+    }, true);
+}
+
+function redirectToEmptyVideo(): browser.webRequest.BlockingResponse {
+    return {
+        redirectUrl: browser.runtime.getURL('/res/videos/dummyVideo.mp4')
+    };
+}
