e058d878c7154fafaad764b86f5df871acb7924f
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-02-16 20:36:15 +0900

    Add postJson

--- app/src/util/util.ts
+++ app/src/util/util.ts
@@ -7,6 +7,19 @@ export async function fetchJson(url: string, init?: RequestInit): Promise<any> {
     return JSON.parse(await (await fetch(url, init)).text());
 }
 
+export async function postJson(url: string, body: any, init?: RequestInit): Promise<any> {
+    Object.assign(init ?? {}, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+        credentials: 'include',
+        mode: 'cors',
+    });
+    return await (await fetch(url, init)).json();
+}
+
 export function sleep(ms: number): Promise<void> {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
