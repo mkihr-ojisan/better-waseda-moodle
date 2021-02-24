b40da1eb528d4397ab91e7a56169def2f6ffc1a1
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-02-24 18:04:10 +0900

    Add polyfill for `browser.contentScript.register`

--- app/src/background.ts
+++ app/src/background.ts
@@ -4,6 +4,8 @@ import { MessengerServer } from './common/util/messenger';
 import { initRemoveLoadingVideo } from './video/removeLoadingVideo/removeLoadingVideo';
 import { initViewInBrowser } from './others/viewInBrowser/viewInBrowser';
 import { initMoreVisibleRemainingTime } from './quiz/moreVisibleRemainingTime/moreVisibleRemainingTime';
+// #!blink_only
+import './common/polyfills/content-script-register';
 
 export const messengerServer = new MessengerServer();
 
