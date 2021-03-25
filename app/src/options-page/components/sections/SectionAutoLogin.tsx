c3870056d11e4e8ee99590c495570a20457025b7
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-03-26 02:59:17 +0900

    Remove debug code

--- app/src/options-page/components/sections/SectionAutoLogin.tsx
+++ app/src/options-page/components/sections/SectionAutoLogin.tsx
@@ -25,8 +25,6 @@ export default function SectionAutoLogin(props: SectionComponentProps): ReactEle
         };
     }
 
-    console.log(loginId);
-
     return (
         <Section titleMessageName="optionsSectionAutoLogin" {...props}>
             <FormGroup>
