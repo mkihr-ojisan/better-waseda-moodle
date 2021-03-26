bbad986f863e287abbefe5d00c96a37bf3fc196a
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-03-26 18:14:34 +0900

    Fix bug
    it appears options page have to contain some text nodes in chrome

--- app/src/options-page/options.tsx
+++ app/src/options-page/options.tsx
@@ -5,4 +5,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Options from './components/Options';
 
+// #!blink_only
+document.body.style.height = '572px';
+
 ReactDOM.render(<Options />, document.getElementById('container'));
