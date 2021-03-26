4042ff99f5b5317fd6f747b8e102d5005fb4a59c
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-03-26 18:23:53 +0900

    Fix bug in chrome

--- app/src/others/syllabus-link-fix/content-script.ts
+++ app/src/others/syllabus-link-fix/content-script.ts
@@ -1,11 +1,17 @@
+const script = document.createElement('script');
+
+script.textContent = `
+
 document.oncontextmenu = null;
 
-document.querySelectorAll('.ctable-main td:nth-child(3) > a').forEach(_elem => {
-    const elem = _elem as HTMLAnchorElement;
-
-    const [, key] = elem.getAttribute('onclick')?.match(/^post_submit\('[^']+', '([^']+)'\)$/) ?? [];
+document.querySelectorAll('.ctable-main td:nth-child(3) > a').forEach(elem => {
+    const [, key] = elem.getAttribute('onclick')?.match(/^post_submit\\('[^']+', '([^']+)'\\)$/) ?? [];
     if (!key) return;
 
-    elem.href = `https://www.wsl.waseda.jp/syllabus/JAA104.php?pKey=${key}`;
+    elem.href = \`https://www.wsl.waseda.jp/syllabus/JAA104.php?pKey=\${key}\`;
     elem.removeAttribute('onclick');
 });
+
+`;
+
+document.head.appendChild(script);
