5057e0afd96caa9e56553fb99180e4926b540514
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-04-05 09:44:30 +0900

    Add support for multiple class periods

--- app/src/common/waseda/course/course-registration.ts
+++ app/src/common/waseda/course/course-registration.ts
@@ -71,6 +71,10 @@ export async function fetchCourseRegistrationInfo(): Promise<CourseRegistrationI
         '通年': 'full_year',
         '春学期': 'spring_semester',
         '秋学期': 'fall_semester',
+        '春クォーター': 'spring_quarter',
+        '夏クォーター': 'summer_quarter',
+        '秋クォーター': 'fall_quarter',
+        '冬クォーター': 'winter_quarter',
     };
     const days: Record<string, DayOfWeek> = {
         '月': 'monday',
@@ -99,26 +103,37 @@ export async function fetchCourseRegistrationInfo(): Promise<CourseRegistrationI
             console.warn(`unknown term '${td[0].innerText.trim()}'`);
             continue;
         }
-        const day = days[td[1].innerText.trim()];
-        if (!day) {
-            console.warn(`unknown day '${day}`);
+
+        const day = getTextFromElement(td[1]).trim().split('\n').map(d => days[d]);
+        const period = getTextFromElement(td[2]).trim().split('\n').map(p => p.split('～').map(p => periods[p]));
+        const termDayPeriods = [];
+        if (day.length !== period.length) {
+            console.warn('day.length and period.length are not equal');
             continue;
         }
-        const period = td[2].innerText.trim().split('～').map(p => periods[p]);
-        if (period.length === 0 || period.length > 2 || period.some(p => p === undefined)) {
-            console.warn(`unknown period '${td[2].innerText.trim()}'`);
-            continue;
-        }
-        const termDayPeriods = [{
-            term,
-            dayPeriod: {
-                day,
-                period: {
-                    from: period[0],
-                    to: period[1] ?? period[0],
+
+        for (let i = 0; i < day.length; i++) {
+            if (!day[i]) {
+                console.warn('unknown day');
+                continue;
+            }
+            if (period[i].length === 0 || period[i].length > 2 || period[i].some(p => p === undefined)) {
+                console.warn('unknown period');
+                continue;
+            }
+
+            termDayPeriods.push({
+                term,
+                dayPeriod: {
+                    day: day[i],
+                    period: {
+                        from: period[i][0],
+                        to: period[i][1] ?? period[i][0],
+                    },
                 },
-            },
-        }];
+            });
+        }
+
         const school = td[3].innerText.trim();
         const note = td[4].innerText.trim();
         const name = td[5].innerText.trim();
