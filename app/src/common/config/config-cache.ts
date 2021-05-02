c9858dc43dd3995b7371c427110a98d72b88543b
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-05-03 02:13:48 +0900

    Add config sync

--- app/src/common/config/config-cache.ts
+++ app/src/common/config/config-cache.ts
@@ -1,12 +1,12 @@
 // キャッシュして同期関数にしたgetConfig, setConfig, removeConfig
 
 import { InternalError } from '../error';
-import { ConfigKey, ConfigValue, defaultValue, storage } from './config';
+import { ConfigKey, ConfigValue, defaultValue, getStorage } from './config';
 
 let cache: { [key: string]: any; } | undefined;
 
 export async function initConfigCache(): Promise<void> {
-    cache = await storage.get(undefined);
+    cache = await (await getStorage()).get(undefined);
 
     browser.storage.onChanged.addListener(changes => {
         if (!cache) throw new InternalError('cache is not initialized');
@@ -25,11 +25,11 @@ export function setConfigCache<T extends ConfigKey>(key: T, value: ConfigValue<T
     if (!cache) throw new InternalError('cache is not initialized');
 
     cache[key] = value;
-    storage.set({ [key]: value });
+    (async () => (await getStorage()).set({ [key]: value }))();
 }
 export function removeConfigCache<T extends ConfigKey>(key: T): void {
     if (!cache) throw new InternalError('cache is not initialized');
 
     delete cache[key];
-    storage.remove([key]);
+    (async () => (await getStorage()).remove([key]))();
 }
---
+++ app/src/common/config/sync.ts
@@ -0,0 +1,40 @@
+import equal from 'fast-deep-equal';
+
+const CONFIG_SYNC_ENABLED_CONFIG_KEY = 'config.sync.enabled';
+
+export async function isConfigSyncEnabled(): Promise<boolean> {
+    return (await browser.storage.local.get(CONFIG_SYNC_ENABLED_CONFIG_KEY))[CONFIG_SYNC_ENABLED_CONFIG_KEY] ?? false;
+}
+
+export async function enableConfigSync(mode: 'discard_local' | 'force_upload'): Promise<void> {
+    if (await isConfigSyncEnabled()) return;
+
+    if (mode === 'force_upload') {
+        await browser.storage.sync.set(await browser.storage.local.get());
+    }
+
+    await browser.storage.local.clear();
+    await browser.storage.local.set({ [CONFIG_SYNC_ENABLED_CONFIG_KEY]: true });
+
+    browser.runtime.reload();
+}
+
+export async function disableConfigSync(): Promise<void> {
+    if (!await isConfigSyncEnabled()) return;
+
+    await browser.storage.local.set(await browser.storage.sync.get());
+    await browser.storage.local.set({ [CONFIG_SYNC_ENABLED_CONFIG_KEY]: false });
+
+    browser.runtime.reload();
+}
+
+export async function checkConflictWhenEnablingConfigSync(): Promise<boolean> {
+    const [local, sync] = await Promise.all([browser.storage.local.get(), browser.storage.sync.get()]);
+    if (Object.keys(sync).length === 0) {
+        return false;
+    } else if (equal(local, sync)) {
+        return false;
+    } else {
+        return true;
+    }
+}
