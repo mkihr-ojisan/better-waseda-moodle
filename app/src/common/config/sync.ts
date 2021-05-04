d475d5c7a98599a4d06b9f4275a110891be08308
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-05-04 12:28:01 +0900

    Change condition for `enableConfigSyncIfFirstRun`

--- app/src/common/config/sync.ts
+++ app/src/common/config/sync.ts
@@ -45,9 +45,9 @@ export async function checkConflictWhenEnablingConfigSync(): Promise<boolean> {
 }
 
 export async function enableConfigSyncIfFirstRun(): Promise<void> {
-    const [local, sync] = await Promise.all([browser.storage.local.get(), browser.storage.sync.get()]);
-    if (Object.keys(local).length === 0 && Object.keys(sync).length === 0) {
-        // storage.localもstorage.syncも空だったら初めて拡張機能が実行されたとみなす
+    const local = await browser.storage.local.get();
+    if (Object.keys(local).length === 0) {
+        // storage.localが空だったら初めて拡張機能が実行されたとみなす
         await enableConfigSync('discard_local');
     }
 }
