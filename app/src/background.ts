008283381199aaca4cfd27978f35f1fdc7a51ad1
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-07-03 19:55:32 +0900

    Move files

--- app/src/background.ts
+++ app/src/background.ts
@@ -3,7 +3,7 @@ import { initConfigCache } from './common/config/config-cache';
 import { MessengerServer } from './common/util/messenger';
 import { initRemoveLoadingVideo } from './others/remove-loading-video/remove-loading-video';
 import { initViewInBrowser } from './others/view-in-browser/view-in-browser';
-import { initMoreVisibleRemainingTime } from './others/more-visible-remaining-time/more-visible-remaining-time';
+import { initMoreVisibleRemainingTime } from './quiz/more-visible-remaining-time/more-visible-remaining-time';
 import { initCourseOverview } from './course-overview/background-script';
 import { fetchCourseList, setHiddenFromCourseList } from './common/waseda/course/course-list';
 import { initDisableRateLimit } from './others/disable-rate-limit/disable-rate-limit';
