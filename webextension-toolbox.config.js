fecedf197d3741087a2c54c535ef895263824f1e
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-07-13 18:56:49 +0900

    Update webextension-toolbox

--- webextension-toolbox.config.js
+++ webextension-toolbox.config.js
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { resolve } = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
-const WebextensionPlugin = require('webpack-webextension-plugin');
 const { readFileSync } = require('fs');
 const webpack = require('webpack');
 
@@ -15,13 +14,15 @@ module.exports = {
         } else {
             config.optimization = {
                 minimize: true,
-                minimizer: [new TerserPlugin({
-                    terserOptions: {
-                        format: {
-                            comments: false,
+                minimizer: [
+                    new TerserPlugin({
+                        terserOptions: {
+                            format: {
+                                comments: false,
+                            },
                         },
-                    },
-                })],
+                    }),
+                ],
             };
         }
 
@@ -29,11 +30,11 @@ module.exports = {
 
         config.module.rules.push({
             test: /\.tsx?$/,
-            loaders: [
+            use: [
                 'ts-loader',
                 {
                     loader: require.resolve('webpack-preprocessor-loader'),
-                    options: {            
+                    options: {
                         debug: dev,
                         directives: {
                             blink_only: vendor !== 'firefox',
@@ -44,31 +45,34 @@ module.exports = {
             ],
             exclude: [/node_modules/],
         });
-       
 
-        config.plugins.push(new webpack.DefinePlugin({
-            __VENDOR__: JSON.stringify(vendor),
-        }));
+        config.plugins.push(
+            new webpack.DefinePlugin({
+                __VENDOR__: JSON.stringify(vendor),
+            })
+        );
 
-        config.plugins.find(p => p instanceof WebextensionPlugin).autoreload = false;
+        config.plugins.find((p) => p.constructor.name === 'WebextensionPlugin').autoreload = false;
 
         // Important: return the modified config
         return config;
     },
-    copyIgnore: [ '**/*.js', '**/*.json', '**/*.ts', '**/*.tsx' ],
+    copyIgnore: ['**/*.js', '**/*.json', '**/*.ts', '**/*.tsx'],
 };
 
 function generateWebpackEntry() {
     const entryPoints = getEntryPoints();
-    
+
     const regex = new RegExp(`^${resolve('app')}/(.*).tsx?$`);
 
-    return Object.fromEntries(entryPoints.map(entryPoint => {
-        console.assert(regex.test(entryPoint));
+    return Object.fromEntries(
+        entryPoints.map((entryPoint) => {
+            console.assert(regex.test(entryPoint));
 
-        const outputFile = entryPoint.replace(regex, '$1');
-        return [outputFile, entryPoint];
-    }));
+            const outputFile = entryPoint.replace(regex, '$1');
+            return [outputFile, entryPoint];
+        })
+    );
 }
 
 function getEntryPoints() {
@@ -80,17 +84,17 @@ function getEntryPoints() {
 }
 
 function getContentScriptPathsFromManifest() {
-    const manifest = JSON.parse(readFileSync('app/manifest.json', {encoding: 'utf-8'}));
+    const manifest = JSON.parse(readFileSync('app/manifest.json', { encoding: 'utf-8' }));
     const jsFiles = [];
     for (const contentScript of manifest.content_scripts) {
         if (contentScript.js) {
             jsFiles.push(...contentScript.js);
         }
     }
-    return jsFiles.map(f => resolve('app', f).replace(/.js$/, '.ts'));
+    return jsFiles.map((f) => resolve('app', f).replace(/.js$/, '.ts'));
 }
 
 function getEntryPointsFromJson() {
-    const entryPoints = JSON.parse(readFileSync('entry-points.json', {encoding: 'utf-8'}));
-    return entryPoints.map(f => resolve('app', f));
+    const entryPoints = JSON.parse(readFileSync('entry-points.json', { encoding: 'utf-8' }));
+    return entryPoints.map((f) => resolve('app', f));
 }
