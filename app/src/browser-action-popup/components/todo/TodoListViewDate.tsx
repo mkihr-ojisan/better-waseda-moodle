3e15fec1f4635783e24619eac8f9ebfa696df538
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-08-15 22:00:45 +0900

    Fix style of `TodoListViewDate`

--- app/src/browser-action-popup/components/todo/TodoListViewDate.tsx
+++ app/src/browser-action-popup/components/todo/TodoListViewDate.tsx
@@ -1,4 +1,5 @@
 import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
 import { TodoItem } from '../../../common/todo-list/todo';
@@ -11,11 +12,19 @@ export type TodoListViewDateProps = {
     onRefreshListRequest: () => void;
 };
 
+const useStyles = makeStyles(() => ({
+    date: {
+        paddingBottom: '0 !important',
+    },
+}));
+
 export default React.memo(function TodoListViewDate(props: TodoListViewDateProps) {
+    const classes = useStyles();
+
     return (
         <>
-            <Grid item>
-                <Typography variant="body1">
+            <Grid item className={classes.date}>
+                <Typography variant="body2">
                     {props.date
                         ? format(props.date, browser.i18n.getMessage('dateFormatMonthDateDay'))
                         : browser.i18n.getMessage('popupIndefinite')}
