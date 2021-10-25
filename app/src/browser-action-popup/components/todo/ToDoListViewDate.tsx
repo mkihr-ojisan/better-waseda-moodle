0f190f8bc6e9fc0c6946af6a9bd944435a8c7c48
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-10-26 03:07:03 +0900

    Change color of past dates

--- app/src/browser-action-popup/components/todo/ToDoListViewDate.tsx
+++ app/src/browser-action-popup/components/todo/ToDoListViewDate.tsx
@@ -6,6 +6,8 @@ import { ToDoItem } from '../../../common/todo-list/todo';
 import { format } from '../../../common/util/date';
 import { AlertColor } from '../../../common/util/types';
 import ToDoListViewItem from './ToDoListViewItem';
+import { isSameDay } from 'date-fns';
+import clsx from 'clsx';
 
 export type ToDoListViewDateProps = {
     date?: Date;
@@ -14,19 +16,23 @@ export type ToDoListViewDateProps = {
     handleShowSnackbar: (message: string, severity: AlertColor) => void;
 };
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
     date: {
         paddingBottom: '0 !important',
+    },
+    past: {
+        color: theme.palette.error.main,
     },
 }));
 
 export default React.memo(function ToDoListViewDate(props: ToDoListViewDateProps) {
     const classes = useStyles();
+    const isPast = props.date && !isSameDay(new Date(), props.date) && props.date < new Date();
 
     return (
         <>
             <Grid item className={classes.date}>
-                <Typography variant="body2">
+                <Typography variant="body2" className={clsx(isPast && classes.past)}>
                     {props.date
                         ? format(props.date, browser.i18n.getMessage('dateFormatMonthDateDay'))
                         : browser.i18n.getMessage('popupIndefinite')}
