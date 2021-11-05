b8b720b8841034bfc22fd306113ab72413a0ece1
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-11-05 14:02:16 +0900

    Use `process.env`

--- app/src/common/polyfills/useMediaQuery.ts
+++ app/src/common/polyfills/useMediaQuery.ts
@@ -1,8 +1,9 @@
 import { Theme, useMediaQuery as muiUseMediaQuery } from '@mui/material';
 
 export function useMediaQuery(query: string | ((theme: Theme) => string)): boolean {
-    // #!blink_only
-    return muiUseMediaQuery(query);
-    // #!firefox_only
-    return muiUseMediaQuery(query, { matchMedia: window.matchMedia.bind(window) } as {});
+    if (process.env.VENDOR === 'firefox') {
+        return muiUseMediaQuery(query, { matchMedia: window.matchMedia.bind(window) } as {});
+    } else {
+        return muiUseMediaQuery(query);
+    }
 }
