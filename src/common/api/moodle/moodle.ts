3301cc03a0d55c6ee1b5e4b213c39ed9ae03a856
mkihr-ojisan <mkihr.ojisan@gmail.com>
2024-05-16 17:32:36 +0900

    update: API呼び出し時にセッションが切れていたら再ログインするようにした

--- src/common/api/moodle/moodle.ts
+++ src/common/api/moodle/moodle.ts
@@ -2,6 +2,7 @@ import { assertExtensionContext } from "@/common/util/context";
 import { getSessionKey } from "../../auto-login/session-key-cache";
 import { postJSON } from "../../util/fetch";
 import { limitRate } from "../../util/limit-rate";
+import { doAutoLogin } from "@/common/auto-login/auto-login";
 
 assertExtensionContext("background");
 
@@ -19,7 +20,10 @@ export type MoodleErrorResponse = { error: true; exception: { message: string } 
  */
 export const callMoodleAPI = limitRate(
     1000,
-    async <T, U>(requests: MoodleRequest<T>[]): Promise<MoodleSuccessResponse<U>> => {
+    async <T, U>(
+        requests: MoodleRequest<T>[],
+        options: { noRetryOnSessionExpired?: boolean } = {}
+    ): Promise<MoodleSuccessResponse<U>> => {
         const response = await postJSON<MoodleResponse<U>>(
             `https://wsdmoodle.waseda.jp/lib/ajax/service.php?sesskey=${await getSessionKey()}`,
             requests.map((r, index) => ({ index, ...r }))
@@ -27,7 +31,22 @@ export const callMoodleAPI = limitRate(
 
         if ("error" in response) throw Error(response.error);
         for (const r of response) {
-            if (r.error) throw Error(r.exception.message);
+            if (r.error) {
+                if (
+                    "exception" in r &&
+                    typeof r.exception === "object" &&
+                    r.exception &&
+                    "errorcode" in r.exception &&
+                    r.exception.errorcode === "servicerequireslogin" &&
+                    !options.noRetryOnSessionExpired
+                ) {
+                    // セッションキーが期限切れの場合は再ログインしてリトライ
+                    await doAutoLogin({ skipCheck: true });
+                    return callMoodleAPI(requests, { noRetryOnSessionExpired: true });
+                } else {
+                    throw Error(r.exception.message);
+                }
+            }
         }
 
         return response as MoodleSuccessResponse<U>;
